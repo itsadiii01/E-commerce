102d3ede601d113e3a4a2e0a6db36b9d5ddeb3fb
Aditya Vikram Singh <124037129+itsadiii01@users.noreply.github.com>
2024-04-23 02:39:09 +0530

    Update SearchInput.js

--- client/src/component/Form/SearchInput.js
+++ client/src/component/Form/SearchInput.js
@@ -10,7 +10,7 @@ const SearchInput = () => {
     e.preventDefault();
     try {
       const { data } = await axios.get(
-        `${process.env.REACT_APP_API}/api/v1/product/search/${values.keyword}`
+        `https://ecom-server-production-87e0.up.railway.app/api/v1/product/search/${values.keyword}`
       );
       setValues({ ...values, results: data });
       navigate("/search");
